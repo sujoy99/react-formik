ed57440b1fda3b169871e89241277cef820cdc92
sujoy99 <scg.sujoy@gmail.com>
2022-03-22 17:39:55 +0600

    field component using render props pattern(address field)

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -5,7 +5,9 @@ import * as Yup from 'yup'
 const initialValues = {
 	name: 'Vishwas',
 	email: '',
-	channel: ''
+	channel: '',
+	comments: '',
+	address: '',
 }
 
 const onSubmit = values => {
@@ -17,50 +19,82 @@ const validationSchema = Yup.object({
 	email: Yup.string()
 		.email('Invalid email format')
 		.required('Required'),
-	channel: Yup.string().required('Required')
+	channel: Yup.string().required('Required'),
+	address: Yup.string().required('Required')
 })
 
 const YoutubeForm = () => {
 
 	return (
 		<Formik
-		initialValues={initialValues}
-		validationSchema={validationSchema}
-		onSubmit={onSubmit}
+			initialValues={initialValues}
+			validationSchema={validationSchema}
+			onSubmit={onSubmit}
 		>
-		<Form>
-			<div className='form-control'>
-				<label htmlFor='name'>Name</label>
-				<Field
-					type='text'
-					id='name'
-					name='name'
-				/>
-				<ErrorMessage name='name'/>
-			</div>
+			<Form>
+				<div className='form-control'>
+					<label htmlFor='name'>Name</label>
+					<Field
+						type='text'
+						id='name'
+						name='name'
+					/>
+					<ErrorMessage name='name' />
+				</div>
 
-			<div className='form-control'>
-				<label htmlFor='email'>E-mail</label>
-				<Field
-					type='email'
-					id='email'
-					name='email'
-				/>
-				<ErrorMessage name='email'/>
-			</div>
+				<div className='form-control'>
+					<label htmlFor='email'>E-mail</label>
+					<Field
+						type='email'
+						id='email'
+						name='email'
+					/>
+					<ErrorMessage name='email' />
+				</div>
 
-			<div className='form-control'>
-				<label htmlFor='channel'>Channel</label>
-				<Field
-					type='text'
-					id='channel'
-					name='channel'
-				/>
-				<ErrorMessage name='channel'/>
-			</div>
+				<div className='form-control'>
+					<label htmlFor='channel'>Channel</label>
+					<Field
+						type='text'
+						id='channel'
+						name='channel'
+						placeholder='enter channel name'
+					/>
+					<ErrorMessage name='channel' />
+				</div>
 
-			<button type='submit'>Submit</button>
-		</Form>
+				<div className='form-control'>
+					<label htmlFor='comments'>Comments</label>
+					<Field
+						as='textarea'
+						// component='textarea'
+						id='comments'
+						name='comments'
+						// validate={validateComments}
+					/>
+					<ErrorMessage name='comments' />
+				</div>
+
+				<div className='form-control'>
+              <label htmlFor='address'>Address</label>
+              <Field name='address'>
+                {(props) => {
+									console.log('render props',props)
+									const { field, form, meta } = props
+                  return (
+                    <div>
+                      <input type='text' id="address" {...field} />
+                      {meta.touched && meta.error ? (
+                        <div>{meta.error}</div>
+                      ) : null}
+                    </div>
+                  )
+                }}
+              </Field>
+            </div>
+
+				<button type='submit'>Submit</button>
+			</Form>
 		</Formik>
 	)
 }
