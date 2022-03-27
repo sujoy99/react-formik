f48f67e03370a93ba1c1df1f4021c287f0e4fd58
sujoy99 <scg.sujoy@gmail.com>
2022-03-27 17:33:04 +0600

    radio button

--- formik-control/src/components/FormikContainer.js
+++ formik-control/src/components/FormikContainer.js
@@ -6,22 +6,32 @@ import FormikControl from './FormikControl'
 const FormikContainer = () => {
 
 	const dropdownOptions = [
-    { key: 'Select an option', value: '' },
-    { key: 'Option 1', value: 'option1' },
-    { key: 'Option 2', value: 'option2' },
-    { key: 'Option 3', value: 'option3' }
-  ]
+		{ key: 'Select an option', value: '' },
+		{ key: 'Option 1', value: 'option1' },
+		{ key: 'Option 2', value: 'option2' },
+		{ key: 'Option 3', value: 'option3' }
+	]
+
+	const radioOptions = [
+		{ key: 'Option 1', value: 'rOption1' },
+		{ key: 'Option 2', value: 'rOption2' },
+		{ key: 'Option 3', value: 'rOption3' }
+	]
+
 	const initialValues = {
 		email: '',
 		description: '',
 		selectOption: '',
+		// radioOption: 'rOption3' 
+		radioOption: 'rOption3',
 	}
 	const validationSchema = Yup.object({
 		email: Yup.string()
 			.email('Invalid email format')
 			.required('Required'),
-			description: Yup.string().required('Required'),
-			selectOption: Yup.string().required('Required'),
+		description: Yup.string().required('Required'),
+		selectOption: Yup.string().required('Required'),
+		radioOption: Yup.string().required('Required'),
 	})
 	const onSubmit = values => {
 		console.log('Form data', values)
@@ -42,22 +52,33 @@ const FormikContainer = () => {
 						name='email'
 					/>
 					<FormikControl
-            control='textarea'
-            label='Description'
-            name='description'
-          />
+						control='textarea'
+						label='Description'
+						name='description'
+					/>
 					<FormikControl
-            control='select'
-            label='Select a topic'
-            name='selectOption'
-            options={dropdownOptions}
-          />
+						control='select'
+						label='Select a topic'
+						name='selectOption'
+						options={dropdownOptions}
+					/>
+					{/* practice  */}
 					{/* <FormikControl
-            control='radio'
-            label='Radio topic'
-            name='radioOption'
-            options={radioOptions}
-          /> */}
+						control='radio'
+						label='Radio topic'
+						name='radioOption'
+						options={radioOptions}
+						// value={{ key: 'Option 3', value: 'rOption3' }}
+						value={formik.values.radioOption}
+						setFieldValue={(val) => { console.log("o", val); formik.setFieldValue("radioOption", val)}}
+					/> */}
+					{/* ends  */}
+					<FormikControl
+						control='radio'
+						label='Radio topic'
+						name='radioOption'
+						options={radioOptions}
+					/>
 					{/* <FormikControl
             control='checkbox'
             label='Checkbox topics'
--- formik-control/src/components/RadioButtons.js
+++ formik-control/src/components/RadioButtons.js
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
 
 const RadioButtons = (props) => {
-	const { label, name, options, ...rest } = props
+	console.log("first", props)
+	const { label, name, options, setFieldValue, ...rest } = props
+	const [state, setState] = useState(props.value)
+	console.log("state", state);
 	return (
 		<div className='form-control'>
 			<label>{label}</label>
 			<Field name={name} >
 				{({ field }) => {
+					// field.value = props.value
+					console.log("in", field);
 					return options.map(option => {
 						return (
 							<React.Fragment key={option.key}>
-								<input
+								{/* <input
 									type='radio'
 									id={option.value}
 									{...field}
 									{...rest}
 									value={option.value}
-									checked={field.value === option.value}
-								/>
+									
+									// checked={field.value === props.value}
+									checked={state === option.value}
+									onChange={(e) => {
+										console.log("a", e)
+										setState(e.target.value)
+										setFieldValue(e.target.value)
+										
+										// field.value=e.target
+									}}
+								/> */}
+								<input
+                  type='radio'
+                  id={option.value}
+                  {...field}
+                  {...rest}
+                  value={option.value}
+                  checked={field.value === option.value}
+                />
 								<label htmlFor={option.value}>{option.key}</label>
 							</React.Fragment>
 						)
